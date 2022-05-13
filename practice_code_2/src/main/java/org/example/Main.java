b1732d223b80ad4e05d7cac4967f26a4c104a3aa
Kevin McGoldrick <kevin@kevinmcgoldrick.com>
2022-05-13 16:57:19 -0400

    anagram checker

--- practice_code_2/src/main/java/org/example/Main.java
+++ practice_code_2/src/main/java/org/example/Main.java
@@ -5,6 +5,14 @@ import java.util.*;
 
 import static java.lang.System.in;
 
+/**
+ * Hacker Rank Practice
+ *
+ * Purpose: Basic Java practice - these questions are easy and generally related to Strings
+ *
+ * @author Kevin McGoldrick
+ * @version 1.0 5/13/22
+ */
 public class Main {
     public static void main(String[] args) {
 
@@ -19,8 +27,8 @@ public class Main {
         //Solution1();
 
         //Solution2();
-        //Solution3();
-        Solution4();
+        Solution3();
+        //Solution4();
 
     }
 
@@ -56,7 +64,7 @@ public class Main {
 
     }
 
-    public static class Priorities{
+    public static class Priorities{  // takes formatted input string - limited value question not complete
         public static   List<Student> getStudents(List<String> events){
         List<Student> students = new ArrayList<>();
 
@@ -87,7 +95,7 @@ public class Main {
 
     }
 
-    public static void Solution1(){
+    public static void Solution1(){  // prints a substring of a string given start and end indexes
         Scanner in = new Scanner(System.in);
         String S = in.next();
         int start = in.nextInt();
@@ -116,27 +124,27 @@ public class Main {
 
     }
 
-    public static void Solution3(){
-        String string = "madam";
-        String isPaln = "No";
+    public static void Solution3(){  // palindrome checker input is placed into two arrays, forward and backward and compared, prints 'yes' if palindrome
+        String string = "madamm";  // test input
+        String isPaln = "No";       // method variables
         int arraryLen = string.length();
         int revIndex = arraryLen -1;
 
-        System.out.println( string.equals( new StringBuilder(string).reverse().toString())
-                ? "Yes" : "No" );
+//        System.out.println( string.equals( new StringBuilder(string).reverse().toString())
+//                ? "Yes" : "No" );  // this was a copied solution... only two lines!!!  not my answer till I learn more.
 
 
-        String [] arrayFwd = new String[arraryLen+1];
+        String [] arrayFwd = new String[arraryLen+1]; // create forward and reverse array variables based on the length of the input string
         String [] arrayRev = new String[arraryLen+1];
 
-        for (int i = 0; i < arraryLen; i++) {
+        for (int i = 0; i < arraryLen; i++) {  // iterate and load the forward array and the reverse array in the same loop
             arrayFwd[i] = string.substring(i,i+1);
             arrayRev[revIndex] = arrayFwd[i];
             revIndex --;
         }
 
-        for (int i = 0; i < arraryLen / 2; i++) {
-            if(arrayFwd[i].equals(arrayRev[i])){
+        for (int i = 0; i < arraryLen / 2; i++) {  // since we now have parallel arrays, we check that each is equal at each index
+            if(arrayFwd[i].equals(arrayRev[i])){   // the loop only tests half of the array length since contents are mirror images of each other
                 isPaln = "Yes";
             }else {
                 isPaln = "No";
@@ -146,61 +154,61 @@ public class Main {
         System.out.println(isPaln);
         }
 
-    public static void Solution4(){
-        String a = "";
+    public static void Solution4(){ // Anagram checker
+        String a = "";  // is string a an anagram of string b?
         String b = "";
-        String isAnagram = "Not Anagrams";
+        String isAnagram = "Not Anagrams";  // default output - Not Anagrams
 
-//        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);  // commented out stdin
 //        a = sc.next();
 //        b = sc.next();
 
-       a = "anagramrmm";
+       a = "anagramrmm";  // set variables for testing
         b = "marganarmm";
 
-        a = a.toLowerCase(Locale.ROOT);
+        a = a.toLowerCase(Locale.ROOT);  // checker is case-insensitive, so each input is converted to lower case
         b = b.toLowerCase(Locale.ROOT);
 
-        if(a == "" || b == "" || a == null || b == null){
+        if(a == "" || b == "" || a == null || b == null){  // short circuit if input is empty or null
             System.out.println(isAnagram);
             return;
         }
 
-        if(a.length() != b.length()){
+        if(a.length() != b.length()){  // short circuit if inputs are not equal length
             System.out.println(isAnagram);
             return;
         }
 
-        Map<String, Integer> mapB = new HashMap<String, Integer>();
-        for (int i = 0; i < b.length(); i++) {
+        Map<String, Integer> mapB = new HashMap<String, Integer>();  // create hashmap to load string B
+        for (int i = 0; i < b.length(); i++) {  // iterate through string B
             if (! mapB.containsKey(b.substring(i,i+1))){
-               mapB.put((b.substring(i,i+1)), 1);
+               mapB.put((b.substring(i,i+1)), 1); // if the current char in string B is not in the hashmap, add it with a key of the char and initial value of 1
             }else {
-                int numOfLetter = mapB.get(b.substring(i,i+1));
+                int numOfLetter = mapB.get(b.substring(i,i+1));  // else char is already in hashmap, therefore increment the count of that char by 1
                 numOfLetter ++;
                 mapB.put((b.substring(i,i+1)), numOfLetter);
             }
 
         }
 
-        for (int i = 0; i < a.length(); i++) {
-            String letter = a.substring(i,i+1);
-            if(mapB.containsKey(letter)){
-                int numOfLettersLeft = mapB.get(letter);
-                if (numOfLettersLeft == 0){
+        for (int i = 0; i < a.length(); i++) {  // iterate through string A and remove chars from the hashmap "inventory"
+            String letter = a.substring(i,i+1);  // get the first char from string A to test
+            if(mapB.containsKey(letter)){  // is the char in string A
+                int numOfLettersLeft = mapB.get(letter);  // check to see if there is anymore inventory for that char in the hashmap
+                if (numOfLettersLeft == 0){  // if no more inventory for char being tested, return - Not Anagrams
                     System.out.println(isAnagram);
                     return;
                 }else{
-                    numOfLettersLeft --;
+                    numOfLettersLeft --;  // if inventory of char being tested remain, decrement inventory by 1
                     mapB.put(letter,numOfLettersLeft);
                 }
             }else{
-                System.out.println(isAnagram);
+                System.out.println(isAnagram);  // a char from string A does not exist in hashmap... Not Anagrams
                 return;
             }
 
         }
-        System.out.println("Anagrams");
+        System.out.println("Anagrams"); // if we get to this line of code, strings A and B are anagrams.
 
     }
 
